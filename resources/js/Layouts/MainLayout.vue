ee55b9c8a7e49c6342d785cffded1a05b3f54c18
eocalix <edwin_calix@hotmail.com>
2023-02-08 15:51:45 -0600

    Init commit

---
+++ resources/js/Layouts/MainLayout.vue
@@ -0,0 +1,7 @@
+<script setup>
+</script>
+<template>
+    <div class="container-fluid py-2 h-100">
+        <slot></slot>
+    </div>
+</template>
---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,15 @@
+<script setup>
+import MainLayout from '../Layouts/MainLayout.vue';
+import { Head } from '@inertiajs/vue3';
+
+</script>
+<script>
+export default {
+    layout: MainLayout,
+}
+</script>
+<template>
+    <Head title="Home"></Head>
+    <h1 class="bg-success">Hola Mundo</h1>
+    <i class="fa-solid fa-ring"></i>
+</template>
